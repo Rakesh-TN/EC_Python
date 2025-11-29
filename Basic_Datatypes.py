fdf98e9ddb1dffd1e8479a8fdaedc25551fe119f
Rakesh TN <tnrakesh68@outlook.com>
2025-11-30 03:48:06 +0530

    Print a Variable With Formating String

---
+++ Basic_Datatypes.py
@@ -0,0 +1,10 @@
+First_Name = 'Rakesh'
+Last_Name = 'TN'
+age = 24.7
+working_status = True
+currently_in = 'DXC Technology'
+
+
+print(f"My name is {First_Name} {Last_Name}. "
+      f"I am {age} years old and currently working status is {working_status}."
+      f" At present, I am associated with {currently_in}.")
